4c72a98d172105162d328c5907f2eabc383bbc7a
ch3atcod3 <ronog711@gmail.com>
2025-07-15 04:59:04 +0300

    Update event_simulation.py

---
+++ event_simulation.py
@@ -0,0 +1,16 @@
+def tick(self):
+    self.current_tick += 1
+    print(f"\n[Tick {self.current_tick}]")
+
+
+    for job in self.queue:
+        job['waiting_time'] += 1
+
+
+    self.apply_priority_aging()
+
+
+    self.remove_expired_jobs()
+
+
+    self.show_status()
